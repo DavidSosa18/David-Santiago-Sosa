455f3ae49dde9c04616894e97db5d2b3143efadb
DavidSosa18 <60710614+DavidSosa18@users.noreply.github.com>
2020-02-05 13:30:23 -0500

    sUMATORIA

---
+++ TAREASUMATORIA.java
@@ -0,0 +1,14 @@
+package tareasumatoria;
+public class TAREASUMATORIA 
+{
+
+   public static void main(String[] args) 
+    {
+        System.out.println("Programa que suma todos los numeros impares desde el 1 hasta 100.000.000");
+        double s=0;
+        s=(100000000+1)/2;
+        s=s*s;
+        System.out.println("La sumatoria de los numeros impares desde el 1 hasta 100.000.000 es de "+s);
+    }
+    
+}
